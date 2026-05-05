fc7c08d362275e3289cb8ce7bf6e3c77a99d5686
Junsu <junsoo122@naver.com>
2026-05-05 18:59:49 +0900

    객체 폭 감지 패키지 수정 및 런치 생성

--- object_width_filter/setup.py
+++ object_width_filter/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'object_width_filter'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # 아래 줄을 추가하여 launch 폴더의 모든 .py 파일을 설치 경로로 복사
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +28,22 @@ setup(
     },
     entry_points={
         'console_scripts': [
+############################################ 런치 실행 코드 ############################################
+            
+## 메인 실행 코드 ---->                   object_width_filter.launch.py
+
+############################################ 개별 실행 코드 ############################################
+
+            'length_filter = object_width_filter.length_filter:main',
+            'no_ground = object_width_filter.no_ground:main',
+            'publish_angle = object_width_filter.publish_angle:main',
+            'z_normalization = object_width_filter.z_normalization:main',
+
+############################################## 테스트 코드 #############################################
+
+            'test_angle = object_width_filter._test_angle_test_publisher:main',
+            'test_joint = object_width_filter._test_joint6_check:main',
+            'test_cam = object_width_filter._test_cam2depth:main',
         ],
     },
 )
